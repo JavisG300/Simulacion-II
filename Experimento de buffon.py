83a9c5466f154b3ade79c1ee3be29c12306d15af
Javier Andrade <javieregonzaleza@gmail.com>
2022-03-03 12:25:58 -0600

    Experimento de buffon primero versión

--- Experimento de buffon.py
+++ Experimento de buffon.py
@@ -1,5 +1,13 @@
 #Experimento de buffon
+from cmath import pi
 import random
-def buffon(N):
-    x = random.random()
-#Si choca la recta crecada con x y y con la recta originasl
+import numpy as np
+def buffon(N,l,d): #l=longitud del alfiler, d=distancia entre las rectas
+    n = 0
+    x = np.random.uniform(0,np.pi,N)
+    theta = np.random.uniform(0,d/2,N)
+    for i in range(N):
+        if theta <= l * np.sin(x)/2:
+            k = k + 1
+    pi = (2 * l * N) /(d * k)
+    return pi
