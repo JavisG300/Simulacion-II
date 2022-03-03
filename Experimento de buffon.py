53f5903552db0f4aae679d1d792a867c2946c96b
Javier Andrade <javieregonzaleza@gmail.com>
2022-03-03 12:33:49 -0600

    Experimento de buffon mas corto

--- Experimento de buffon.py
+++ Experimento de buffon.py
@@ -1,13 +1,12 @@
 #Experimento de buffon
-from cmath import pi
-import random
 import numpy as np
 def buffon(N,l,d): #l=longitud del alfiler, d=distancia entre las rectas
     n = 0
     x = np.random.uniform(0,np.pi,N)
     theta = np.random.uniform(0,d/2,N)
     for i in range(N):
-        if theta <= l * np.sin(x)/2:
-            k = k + 1
-    pi = (2 * l * N) /(d * k)
+        if theta <= l * (np.sin(x)/2):
+            n = n + 1
+    pi = (2*l*N) /(d*n)
     return pi
+print(buffon(100,0.8,1))
