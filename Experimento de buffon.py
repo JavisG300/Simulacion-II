79e15cc23692b63e21a2c3caede5f13d2a1c7e18
Javier Emanuel González Andrade <98623160+JavisG300@users.noreply.github.com>
2022-03-09 19:39:42 -0600

    Experimento de Buffon

--- Experimento de buffon.py
+++ Experimento de buffon.py
@@ -2,10 +2,10 @@
 import numpy as np
 def buffon(N,l,d): #l=longitud del alfiler, d=distancia entre las rectas
     n = 0
+    x = np.random.uniform(0,np.pi,N)
+    theta = np.random.uniform(0,d/2,N)
     for i in range(N):
-        x = np.random.uniform(0,np.pi,N)
-        theta = np.random.uniform(0,d/2,N)
-        if theta <= l*(np.sin(x)/2):
+        if theta <= l*(np.sin(x))/2:
             n = n + 1
     pi = (2*l*N) /(d*n)
     return pi
